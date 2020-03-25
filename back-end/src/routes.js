e9987e4e6fa4ef63ecba9f70abbeb0ff6426ce8a
Eder Gabriel <eder1602@outlook.com>
2020-03-25 13:38:04 -0300

    URLs de Casos

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const connection = require('./database/connection');
 
@@ -9,4 +10,6 @@ const routes = express.Router();
 routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
 
+routes.post("/incidents", IncidentController.create);
+routes.get("/incidents", IncidentController.index);
 module.exports = routes;
